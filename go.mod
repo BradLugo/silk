dbb28781092960121d8e49241ca01e9f93598d83
Brad Lugo <brad@bradlugo.com>
2020-12-23 00:09:47 -0600

    feat: add graphql functionality

--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module webber
 go 1.15
 
 require (
+	github.com/99designs/gqlgen v0.13.0
 	github.com/DeanThompson/ginpprof v0.0.0-20201112072838-007b1e56b2e1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/neo4j/neo4j-go-driver v1.8.3
 	github.com/stretchr/testify v1.6.1
+	github.com/vektah/gqlparser/v2 v2.1.0
 	go.uber.org/zap v1.16.0 // indirect
 )
--- router.go
+++ router.go
@@ -1,9 +1,15 @@
 package webber
 
 import (
+	"net/http"
+	"webber/graph"
+	"webber/graph/generated"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+
 	"github.com/DeanThompson/ginpprof"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func GetGinEngine(debug bool) (r *gin.Engine) {
@@ -16,6 +22,9 @@ func GetGinEngine(debug bool) (r *gin.Engine) {
 
 	r.GET("/health", routeHealth)
 
+	r.POST("/query", graphqlHandler())
+	r.GET("/", playgroundHandler())
+
 	if debug {
 		ginpprof.Wrapper(r)
 	}
@@ -27,4 +36,25 @@ func routeHealth(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"status": "imok",
 	})
+}
+
+// Defining the Playground handler
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// Defining the Graphql handler
+func graphqlHandler() gin.HandlerFunc {
+	// NewExecutableSchema and Config are in the generated.go file
+	// Resolver is in the resolver.go file
+	r := generated.Config{Resolvers: &graph.Resolver{}}
+	h := handler.NewDefaultServer(generated.NewExecutableSchema(r))
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
 }
